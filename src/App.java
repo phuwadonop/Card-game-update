cf4d2a1da730ce1572c3446b9e7e7a900fa1ea4f
phuwadonop <63010774@kmitl.ac.th>
2021-04-18 03:54:55 +0700

    add menu

--- src/App.java
+++ src/App.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
+
 public class App extends Application{
 
     public static void main(String[] args) throws Exception {
@@ -15,9 +16,9 @@ public class App extends Application{
     @Override
     public void start(Stage stage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getResource("Scene.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("MenuScene.fxml"));
         stage.setScene(new Scene(root));
-        stage.setTitle("Slave");
+        stage.setTitle("Slave Menu");
         stage.show();
         
     }
